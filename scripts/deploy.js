03aabbcbee1c1a01cb5417a7aa84f2c3fa381a42
Gonenti <99602725+Gonenti@users.noreply.github.com>
2022-02-23 22:52:47 +0700

    Add files via upload

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,19 @@
+const hre = require("hardhat");
+const ethers = hre.ethers
 
+async function main() {
+    const [signer] = await ethers.getSigners()
+
+    const Donat = await ethers.getContractFactory("Donat", signer)
+    const donat = await Donat.deploy()
+
+    await donat.deployed();
+    console.log(donat.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
